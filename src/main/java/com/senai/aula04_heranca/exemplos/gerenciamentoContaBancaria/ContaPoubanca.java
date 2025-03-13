f2710b3e382bdd1e94060a788a480e5a1a74872e
Ruan Macionil <macinilruan@gmail.com>
2025-03-13 11:10:54 -0300

    aulaJavaPOO  04

--- src/main/java/com/senai/aula04_heranca/exemplos/gerenciamentoContaBancaria/ContaPoubanca.java
+++ src/main/java/com/senai/aula04_heranca/exemplos/gerenciamentoContaBancaria/ContaPoubanca.java
@@ -1,10 +1,15 @@
 package com.senai.aula04_heranca.exemplos.gerenciamentoContaBancaria;
 
 public class ContaPoubanca extends ContaBancaria{
-private double taxaDeRendimento
+private double taxaDeRendimento;
 
 
-    public ContaPoubanca(String titular, double saldo) {
+    public ContaPoubanca(String titular, double saldo, double taxaDeRendimento) {
         super(titular, saldo);
+        this.taxaDeRendimento = taxaDeRendimento;
+    }
+
+    public void aplicarRendimento(){
+        saldo +=(saldo*taxaDeRendimento)/100;
     }
 }
